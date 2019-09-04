f7b613450ea8a709474b8859e496210fb9d5b379
WRobertoc <54857573+WRobertoc@users.noreply.github.com>
2019-09-03 23:15:29 -0300

    Update MainActivity
    Textview clickable for background change color

--- app/src/main/java/com/robertoc/myhelloworld/MainActivity.kt
+++ app/src/main/java/com/robertoc/myhelloworld/MainActivity.kt
@@ -3,14 +3,22 @@ package com.robertoc.myhelloworld
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    val TAG: String = "ShowLogCat"
+    val TAG: String = "Hello world"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.i(TAG, "Hello World")
+
+        hello.setText(TAG)
+
+        hello.setOnClickListener {
+            var n = (0..2).random()
+            val cores = this.resources.getIntArray(R.array.cores)
+            myLayout.setBackgroundColor(cores[n])
+        }
     }
 }
