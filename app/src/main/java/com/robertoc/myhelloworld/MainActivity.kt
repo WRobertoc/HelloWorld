7e371cda50965c1cea249bc46f83e638a9ec9669
WRobertoc <54857573+WRobertoc@users.noreply.github.com>
2019-09-04 08:11:23 -0300

    Update MainActivity
    TextView clickable for hello world in differents languages using JSON

--- app/src/main/java/com/robertoc/myhelloworld/MainActivity.kt
+++ app/src/main/java/com/robertoc/myhelloworld/MainActivity.kt
@@ -4,18 +4,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
-    val TAG: String = "Hello world"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var hello_world = JSONObject("""{"0":"Hello world","1":"Olá mundo","2": "Hola mundo"}""")
 
-        hello.setText(TAG)
 
+        hello.setText("teste")
         hello.setOnClickListener {
+            var r = (0..2).random().toString()
+            var resultado = hello_world.get(r)  as String?
+            hello.setText(resultado)
+
             var n = (0..2).random()
             val cores = this.resources.getIntArray(R.array.cores)
             myLayout.setBackgroundColor(cores[n])
